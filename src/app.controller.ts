844c570145d614566f0359c42968035496e4a8ba
Richard <thanhle@ancient8.gg>
2025-11-21 14:26:54 +0700

    add 2 demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,114 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Request,
+  Post,
+  UseInterceptors,
+  HttpException,
+} from '@nestjs/common';
+import { Request as ExpressRequest } from 'express';
+import {
+  PricingRequirement,
+  X402ApiConfig,
+  X402ApiOptions,
+  X402DynamicPricing,
+  X402Interceptor,
+  X402PaymentService,
+  X402ReqConfig,
+} from 'nestjs-x402';
 
-@Controller()
+@Controller('hello')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(readonly paymentService: X402PaymentService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/static')
+  @UseInterceptors(X402Interceptor)
+  @X402ApiOptions({
+    apiPrices: [
+      {
+        network: 'base',
+        price: '$0.001', // Price in USD
+      },
+    ],
+    description: 'Get a greeting from me!',
+    discoverable: true,
+    inputSchema: {
+      your_name: {
+        type: 'string',
+        description: 'Your name',
+        nullable: false,
+      },
+      number_of_greetings: {
+        type: 'number',
+        description: 'Number of greetings you want',
+      },
+    },
+    outputSchema: {
+      greeting: {
+        type: 'string',
+        description: 'The greeting message',
+      },
+    },
+  })
+  getStaticHello(@Body() { your_name }: { your_name: string }) {
+    return { greeting: `Hello, ${your_name}!` };
+  }
+
+  @Post('/dynamic')
+  @X402ApiOptions({
+    description: 'Get a greeting from me!',
+    discoverable: true,
+    inputSchema: {
+      your_name: {
+        type: 'string',
+        description: 'Your name',
+        nullable: false,
+      },
+      number_of_greetings: {
+        type: 'number',
+        description: 'Number of greetings you want',
+      },
+    },
+    outputSchema: {
+      greetings: {
+        type: 'array',
+        description: 'The greeting messages',
+        items: { type: 'string' },
+      },
+    },
+  })
+  async getDynamicHello(
+    @Request() req: ExpressRequest,
+    @Body()
+    {
+      your_name,
+      number_of_greetings = 1,
+    }: { your_name: string; number_of_greetings: number },
+    @X402ReqConfig() x402Config: X402ApiConfig,
+  ) {
+    const prices: PricingRequirement[] = [
+      { price: `$${1 * number_of_greetings}`, network: 'base' },
+    ];
+    const paymentHeader = req.header('X-PAYMENT');
+    if (!paymentHeader) {
+      throw new X402DynamicPricing({ dynamicPrices: prices });
+    }
+    const paymentRequirements = this.paymentService.getExactPaymentRequirements(
+      x402Config,
+      prices,
+    );
+    const { valid, x402Response } = await this.paymentService.processPayment({
+      paymentRequirements,
+      paymentHeader,
+    });
+    if (!valid) {
+      throw new HttpException(x402Response!, 402);
+    }
+
+    const greetings = Array.from(
+      { length: number_of_greetings },
+      () => `Hello, ${your_name}!`,
+    );
+    return { greetings };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { facilitator } from '@coinbase/x402';
+import { X402Module } from 'nestjs-x402';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    X402Module.registerAsync({
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        x402Version: 1,
+        recipients: [
+          {
+            network: 'base',
+            payTo: configService.get<string>('X402_PAY_TO')!,
+          },
+        ],
+        resource: configService.get<string>('X402_RESOURCE')!,
+        facilitator,
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
